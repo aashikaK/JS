fdc6d4afea0f8639be7ec0135ae9bfcde2ff4280
Aashika <khatiwadaaashika12@gmail.com>
2024-09-11 19:48:34 -0400

    js time

---
+++ variables.js
@@ -0,0 +1,17 @@
+// console.log("Hello World"); 
+// const a=67;
+// const name= "Sera";
+// const date= new Date();
+// console.log(date, date.getDay());
+// var b;
+// b=7;
+// let c;
+// if(true){
+//   var z=6;
+// }
+// console.log(z);   //it is accessible but const and let are not as is scope is within a block only
+
+const h1= document.getElementById("head").innerHTML="Hello Aashika";
+// h1.innerHTML="Hello Aashika";
+console.log(h1);
+
